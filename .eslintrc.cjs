50061d95e61caddc186f321dd804ceea36a1787d
jeromeboileux <wab2com@gmail.com>
2023-11-08 00:51:50 +0100

    🔧 add specific eslint & typescript config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,44 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  extends: ["@remix-run/eslint-config", "@remix-run/eslint-config/node"],
+  extends: [
+    '@remix-run/eslint-config',
+    '@remix-run/eslint-config/node',
+    'plugin:@tanstack/eslint-plugin-query/recommended',
+  ],
+  rules: {
+    'no-restricted-imports': [
+      'error',
+      {
+        patterns: [
+          {
+            group: ['~/features/*/*'],
+            message:
+              '\nImport [feature] from @/features/[feature] instead or use relative import inside the same feature',
+          },
+        ],
+      },
+    ],
+
+    'import/order': [
+      'error',
+      {
+        pathGroups: [
+          {
+            pattern: 'react',
+            group: 'external',
+            position: 'before',
+          },
+          {
+            pattern: '~/**',
+            group: 'parent',
+            position: 'before',
+          },
+        ],
+        pathGroupsExcludedImportTypes: ['react'],
+        groups: ['builtin', 'external', 'internal', 'parent', 'sibling'],
+        'newlines-between': 'always',
+        alphabetize: { order: 'asc', caseInsensitive: true },
+      },
+    ],
+  },
 };
